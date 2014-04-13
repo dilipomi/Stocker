62c4e7ebe30d8419e85145b23a8dafbb5a73dbb7
Yinchu Hui <yhui@yhui-mbp15.local>
2014-04-12 21:54:14 -0700

    UI improve

--- src/com/example/stocker/StockData.java
+++ src/com/example/stocker/StockData.java
@@ -6,14 +6,18 @@ public class StockData {
 	public String price;
 	public String change;
 	public String changePercent;
+	public String low;
+	public String high;
 
 	public StockData(String symbol, String price, String change,
-			String changePercent) {
+			String changePercent, String low, String high) {
 		super();
 		this.symbol = symbol;
 		this.price = price;
 		this.change = change;
 		this.changePercent = changePercent + "%";
+		this.low = low;
+		this.high = high;
 	}
 
 }
